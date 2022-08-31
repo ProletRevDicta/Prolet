acf2ad6415dd43445a1374d9035d43988768631b
a <waosp@proton.me>
2022-08-31 05:47:49 -0400

    列表

--- gen_md.py
+++ gen_md.py
@@ -4,5 +4,5 @@ t=''
 for a in os.walk(sys.path[0]):
     for b in a[2]:
         if b[-3:]in['pdf','PDF']:
-            t='%s[%s](%s)（%s）\n'%(t,'.'.join(b.split('.')[:-1]),'%s/%s'%('/'.join([quote(c)for c in a[0].replace('/home/a/Prolet/','').replace('/home/a/Prolet','').split('/')]),quote(b)),'[TXT文件](%s)'%('%s/%s'%('/'.join([quote(c)for c in a[0].replace('/home/a/Prolet/','').replace('/home/a/Prolet','').split('/')]),quote('%s.txt'%'.'.join(b.split('.')[:-1]))))if os.path.exists('%s/%s.txt'%(a[0],'.'.join(b.split('.')[:-1])))else'暂无')
+            t='%s[%s](%s)（%s）\n\n'%(t,'.'.join(b.split('.')[:-1]),'%s/%s'%('/'.join([quote(c)for c in a[0].replace('/home/a/Prolet/','').replace('/home/a/Prolet','').split('/')]),quote(b)),'[TXT文件](%s)'%('%s/%s'%('/'.join([quote(c)for c in a[0].replace('/home/a/Prolet/','').replace('/home/a/Prolet','').split('/')]),quote('%s.txt'%'.'.join(b.split('.')[:-1]))))if os.path.exists('%s/%s.txt'%(a[0],'.'.join(b.split('.')[:-1])))else'暂无')
 f=open('LIST.md','w+');f.write(t);f.close()
