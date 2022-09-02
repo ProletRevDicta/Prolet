3bfdcb34a490f889531dfe546d77a0d4c01a2cc5
a <waosp@proton.me>
2022-09-01 23:16:14 -0400

    OCR

--- gen_README_repeat.py
+++ gen_README_repeat.py
@@ -27,6 +27,7 @@ for a in fo3:
         elif'电影'in b:ap='电影'
         elif'哲学'in b:ap='哲学'
         elif'马列毛'in b:ap=['马克思','恩格斯','列宁','斯大林','毛泽东']
+        elif'自学'in b:ap=['自学','自然']
         else:ap=b
         if ap not in fo4[len(fo4)-1]:
             if isinstance(ap,list):
