c7099da23558a3fcb1c79b441a5d0ada7b9fa26d
a <waosp@proton.me>
2022-09-26 05:03:57 -0400

    毛澤東選集朝鮮文百毒AI掃描

--- gen_README.py
+++ gen_README.py
@@ -3,7 +3,7 @@ import os,sys
 fo=[]
 for a in os.walk(sys.path[0]):
     for b in a[2]:
-        if b[-3:]in['txt','TXT']:
+        if(b[-3:]in['txt','TXT'])and(a[0].find('档案图')==-1):
             if a[0]not in fo:fo.append(a[0])
 fo=[a for a in fo if a!='/home/a/Prolet']
 for d in fo:
@@ -11,6 +11,6 @@ for d in fo:
 | ------- | ------- |\n'''
     for a in os.walk(d):
         for b in a[2]:
-            if b[-3:]in['txt','TXT']:
+            if(b[-3:]in['txt','TXT'])and(a[0].find('档案图')==-1):
                 t='%s| [%s](%s) | %s |\n'%(t,'.'.join(b.split('.')[:-1]),'%s/%s'%('/'.join([quote(c)for c in a[0].replace('/home/a/Prolet/','').replace('/home/a/Prolet','').split('/')]),quote(b)),'[下载](%s)'%('%s/%s'%('/'.join([quote(c)for c in a[0].replace('/home/a/Prolet/','').replace('/home/a/Prolet','').split('/')]),quote('%s.pdf'%'.'.join(b.split('.')[:-1]))))if os.path.exists('%s/%s.pdf'%(a[0],'.'.join(b.split('.')[:-1])))else'暂无')
     f=open('%s/README.md'%d,'w+');f.write(t);f.close()
