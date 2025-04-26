b31376c365a9b92bcca83dc3b4a9498b082a76eb
a <waosp2014@proton.me>
2025-04-26 16:56:49 +0800

    upload.

--- TQ 化学工业/TP Sci-Hub氯酸钠论文/remove_symbol.py
+++
@@ -1,6 +0,0 @@
-import os,sys
-for a in os.walk(sys.path[0]):
-    for b in a[2]:
-        if('['in b)or(']'in b):
-            print(b)
-            os.rename(b,b.replace('[','(').replace(']',')'))
--- remove_filename_space.py
+++
@@ -1,8 +0,0 @@
-import os,sys
-for a in os.walk(sys.path[0]):
-    for b in a[2]:
-        if b[-3:]=='pdf'or b[-3]=='txt':
-            if b.split('.pdf')[0].split('txt')[0].strip()!=b.split('.pdf')[0].split('txt')[0]:
-                print('\'%s\''%b)
-                os.rename('%s/%s'%(a[0],b),dn:='%s/%s.%s'%(a[0],b.split('.pdf')[0].split('txt')[0].strip(),b.split('.')[-1:][0]))
-                print(dn)
